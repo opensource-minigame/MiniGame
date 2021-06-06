13f6618682442098dcfdb619bae1ac8d1477f176
dostostet <81497374+dostostet@users.noreply.github.com>
2021-06-06 14:28:49 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -37,30 +37,75 @@ app.get('/mineweeper1.html',function(req,res){
 });
 
 app.get('/tetris', function(req,res){
-    res.sendFile(__dirname + "/mineweep/tetris.html")
+    res.sendFile(__dirname + "/tetris.html")
 });
-
+/*
+app.get('/inputtext.html',function(req,res){
+    conn.query('SELECT * FROM Persons', function(err, rows, fields){
+        if(err) console.log(err,rows);
+        else{
+            for(let i=0; i<rows.length; i++){
+                console.log(i+" PersonsID: " + rows[i].PersonID+", "+"Name : "+ rows[i].PersonName+", 학과 : "+rows[i].Majority);
+            }
+        }
+    })
+})*/
 ///////////////////////////////////////////////////////////////
+
 app.post('/input_post', (req,res)=>{
+    
     let PersonName = req.body.PersonName;
     let PersonID = req.body.PersonID;
     let Majority = req.body.Majority;
+/*
+    $(function(){
+        conn.query('SELECT * FROM Persons', function(err, rows, fields){
+            if(err) console.log(err);
+            else{
+                for(let i=0; i<rows.length; i++){
+                    let a = i+" PersonsID: " + rows[i].PersonID+", "+"Name : "+ rows[i].PersonName+", 학과 : "+rows[i].Majority;
+                    $('<tr><td>'+rows[i].PersonID+'</td><td>'+rows[i].PersonName+'</td><td>'+rows[i].Majority+'</td></tr>').appendTo(".table1>tbody")
+                }
 
-    var query = 'select * from Persons';
-    conn.query(query, function(err, rows, fields){
+            }
+        })
+    })*/
+    /*
+    conn.query(query, function(err){
         if(err){console.log('FILE READ FAILED!')}
         else{
             var query = conn.query('insert into Persons (PersonID, PersonName, Majority) values ("'+PersonID+'","'+PersonName+'","'+Majority+'")', 
-            function(err, rows, fields){
+            function(err){
                 if(err){throw err;}
                 else{
                     console.log("Insert Complete!")
                 }
             });
-            var query = conn.query('select * from Persons')
             console.log(query)
         }
-    });
-    res.sendFile("<h1>성공하였습니다!</h1>")
+    });*/
+    //res.sendFile(__dirname+"/sField.html");
+    //res.sendFile(__dirname+"/inputtext.html")
+    conn.query('SELECT * FROM Persons ORDER BY PersonID DESC', function(err, rows, fields){
+        let a =''
+        if(err) console.log(err,rows);
+        else{
+            a = a + '<table>'
+            for(let i=0; i<rows.length; i++){
+                //a += i+" PersonsID: " + rows[i].PersonID+", "+"Name : "+ rows[i].PersonName+", 학과 : "+rows[i].Majority+'<br>';
+                a += '<tr><td>'+i+'</td><td>'+rows[i].PersonID+"</td><td>"+rows[i].PersonName+"</td><td>"+rows[i].Majority+"</td></tr>"
+            }
+            a += '</table>'
+            res.send(a)
+        }
+    })
+
+
     conn.end();
-});
+}); 
+/*
+app.get('/input_post',function(){
+    $(document).ready(function(){
+        $(".tables")
+    })
+})*/
